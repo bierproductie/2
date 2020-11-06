a2cdb244a278c96ac967c3d6e2df698fe06e78dc
kepet19 <kevinmeejach@gmail.com>
2020-11-06 12:11:53 +0100

    Add intial repository

---
+++ src/main/java/dk/bierproductie/opc_ua_client/RunOPC_UA_Client.java
@@ -0,0 +1,9 @@
+package dk.bierproductie.opc_ua_client;
+
+import dk.bierproductie.opc_ua_client.core.OPC_UA_Client;
+
+public class RunOPC_UA_Client {
+    public static void main(String[] args) {
+        new OPC_UA_Client().start();
+    }
+}
