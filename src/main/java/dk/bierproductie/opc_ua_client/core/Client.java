23b685b3fdedab47d7f26bfd548bf8455b1157c4
kepet19 <kevinmeejach@gmail.com>
2020-11-07 17:36:46 +0100

    There should be no codesmells now

--- src/main/java/dk/bierproductie/opc_ua_client/core/Client.java
+++ src/main/java/dk/bierproductie/opc_ua_client/core/Client.java
@@ -9,61 +9,50 @@ import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Client {
-
     private OpcUaClient opcUaClient;
+    private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-    public Client(String endpointURL) throws ConfigException {
-        List<EndpointDescription> endpoints = null;
-        try {
-            endpoints = DiscoveryClient.getEndpoints(endpointURL).get();
-        } catch (InterruptedException | ExecutionException e){
-            System.out.println("Check endpoint or there is not connection to the server");
-        } finally {
-            if (endpoints == null) {
-                throw new ConfigException("Check there is noting wrong with: URL, Connection to the server");
-            }
-        }
-        System.out.println("Connecting to Endpoint: " + endpoints.get(0));
-        OpcUaClientConfigBuilder ocb = new OpcUaClientConfigBuilder();
-        ocb.setEndpoint(endpoints.get(0));
-        try { 
-            opcUaClient = OpcUaClient.create(ocb.build());
-        } catch(UaException e){
-            System.out.println("System, got UaException");
-        }
-        try {
-            opcUaClient.connect().get();
-        } catch(InterruptedException | ExecutionException e){
-            System.out.println("Can't connect to the system, check url, maybe there should be a username?");
-        }
-
+    public Client(String endpointURL) throws ConfigException, InterruptedException {
+        this(endpointURL, null, null);
     }
 
-    public Client(String endpointURL, String username, String password) throws ConfigException {
+    public Client(String endpointURL, String username, String password) throws ConfigException, InterruptedException {
         List<EndpointDescription> endpoints = null;
         try {
             endpoints = DiscoveryClient.getEndpoints(endpointURL).get();
-        } catch (InterruptedException | ExecutionException e){
-            System.out.println("Check endpoint or there is not connection to the server");
-        } finally {
-            if (endpoints == null) {
-                throw new ConfigException("Check there is noting wrong with: URL, Connection to the server");
-            }
+            LOGGER.log(Level.INFO, "Connection to Endpoint: {}", endpoints.get(0));
+        } catch (InterruptedException e){
+            LOGGER.log(Level.WARNING, "InterruptedException: {}", e.toString());
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e){
+            throw new ConfigException("Check endpoint, check connection to the server");
         }
-        System.out.println("Connecting to Endpoint: " + endpoints.get(0));
-        OpcUaClientConfigBuilder ocb = new OpcUaClientConfigBuilder().setIdentityProvider(new UsernameProvider(username,password));
-        ocb.setEndpoint(endpoints.get(0));
+        OpcUaClientConfigBuilder ocb = new OpcUaClientConfigBuilder();
+        if (username != null && !username.equals("")) {
+            ocb.setIdentityProvider(new UsernameProvider(username, password));
+        }
+        
+        if(endpoints == null) {
+            throw new ConfigException("Could not get end points, check connection?");
+        } else {
+            ocb.setEndpoint(endpoints.get(0));
+        }
         try { 
             opcUaClient = OpcUaClient.create(ocb.build());
         } catch(UaException e){
-            System.out.println("System, got UaException");
+            LOGGER.log(Level.WARNING, "Got an UaException: {}", e.toString());
         }
         try {
             opcUaClient.connect().get();
-        } catch(InterruptedException | ExecutionException e){
-            System.out.println("Can't connect to the system, check url, maybe there should be a username?");
+        } catch (InterruptedException e){
+            LOGGER.log(Level.WARNING, "InterruptedException: {}", e.toString());
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e){
+            LOGGER.log(Level.WARNING, "Check endpoint or check connection to the server or wrong user");
         }
     }
 
--- src/test/java/dk/bierproductie/opc_ua_client/core/ClientTest.java
+++ src/test/java/dk/bierproductie/opc_ua_client/core/ClientTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import dk.bierproductie.opc_ua_client.core.*;
 
-public class ClientTest {
+class ClientTest {
     Client client;
     public ClientTest(){
     }
