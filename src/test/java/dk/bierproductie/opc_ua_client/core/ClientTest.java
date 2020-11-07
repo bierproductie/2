ce7b64a2a577db3791e78e148bb9092233e22039
kepet19 <kevinmeejach@gmail.com>
2020-11-07 14:46:23 +0100

    add a test

---
+++ src/test/java/dk/bierproductie/opc_ua_client/core/ClientTest.java
@@ -0,0 +1,18 @@
+package dk.bierproductie.opc_ua_client.core;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import dk.bierproductie.opc_ua_client.core.*;
+
+public class ClientTest {
+    Client client;
+    public ClientTest(){
+    }
+
+    @Test
+    void getClient(){
+        assertThrows(ConfigException.class,() -> new Client("this is an invalid endpoint"));
+    }
+}
