b0456be9191a0c53be91df616221c288b88cd66b
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-07 17:09:49 +0100

    Create StopReasons.java

---
+++ src/main/java/dk/bierproductie/opc_ua_client/enums/StopReasons.java
@@ -0,0 +1,18 @@
+package dk.bierproductie.opc_ua_client.enums;
+
+public enum StopReasons {
+    EMPTY_INVENTORY(10,"Empty Inventory"),
+    MAINTENANCE_NEEDED(11,"Maintenance Needed"),
+    MANUAL_STOP(12, "Manual Stop"),
+    MOTOR_POWER_LOSS(13, "Motor Power Loss"),
+    MANUAL_ABORT(14, "Manual Abort");
+
+    public final int id;
+    public final String output;
+
+    StopReasons(int id, String output) {
+        this.id = id;
+        this.output = output;
+    }
+
+}
