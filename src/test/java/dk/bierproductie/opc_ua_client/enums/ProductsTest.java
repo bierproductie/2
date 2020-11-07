806a51e7caf9518ab24d4b505e64f7f1fb17d081
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-07 21:01:14 +0100

    Create ProductsTest.java

---
+++ src/test/java/dk/bierproductie/opc_ua_client/enums/ProductsTest.java
@@ -0,0 +1,19 @@
+package dk.bierproductie.opc_ua_client.enums;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductsTest {
+
+    @Test
+    void values() {
+        assertNotNull(Products.values());
+    }
+
+    @Test
+    void valueOf() {
+        assertNotNull(Products.valueOf("PILSNER"));
+    }
+
+}
