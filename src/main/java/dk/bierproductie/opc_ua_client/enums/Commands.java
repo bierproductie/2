c3f470453d7a7825ecff77e7970200a82e6484cc
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-07 17:09:38 +0100

    Create Commands.java

---
+++ src/main/java/dk/bierproductie/opc_ua_client/enums/Commands.java
@@ -0,0 +1,16 @@
+package dk.bierproductie.opc_ua_client.enums;
+
+public enum Commands {
+    ZERO("Zero"),
+    RESET("Reset"),
+    START("Start"),
+    STOP("Stop"),
+    ABORT("Abort"),
+    CLEAR("Clear");
+
+    public final String output;
+
+    Commands(String output) {
+        this.output = output;
+    }
+}
