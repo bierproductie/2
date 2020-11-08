5e33a68f650a020863068af72fffc5215d537410
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-08 21:32:16 +0100

    Update Products.java

--- src/main/java/dk/bierproductie/opc_ua_client/enums/Products.java
+++ src/main/java/dk/bierproductie/opc_ua_client/enums/Products.java
@@ -1,16 +1,18 @@
 package dk.bierproductie.opc_ua_client.enums;
 
 public enum Products {
-    PILSNER("Pilsner"),
-    WHEAT("Wheat"),
-    IPA("IPA"),
-    STOUT("Stout"),
-    ALE("Ale"),
-    ALCOHOL_FREE("Alcohol Free");
+    PILSNER("Pilsner", 600),
+    WHEAT("Wheat", 300),
+    IPA("IPA", 150),
+    STOUT("Stout", 200),
+    ALE("Ale", 100),
+    ALCOHOL_FREE("Alcohol Free", 125);
 
     public final String output;
+    public final int speedLimit;
 
-    Products(String output) {
+    Products(String output, int speedLimit) {
         this.output = output;
+        this.speedLimit = speedLimit;
     }
 }
