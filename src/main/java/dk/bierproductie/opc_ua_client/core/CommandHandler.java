1b2fd0d24f658fda837bfb3dedff3d7f6d269fb3
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-09 21:18:24 +0100

    Update CommandHandler.java

--- src/main/java/dk/bierproductie/opc_ua_client/core/CommandHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/core/CommandHandler.java
@@ -1,6 +1,7 @@
 package dk.bierproductie.opc_ua_client.core;
 
 import dk.bierproductie.opc_ua_client.enums.Commands;
+import dk.bierproductie.opc_ua_client.enums.node_enums.CommandNodes;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
@@ -12,26 +13,24 @@ public class CommandHandler {
 
     private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-    private NodeFactory nodeFactory;
     private Client client;
 
-    public CommandHandler(Client client, NodeFactory nodeFactory) {
-        this.nodeFactory = nodeFactory;
+    public CommandHandler(Client client) {
         this.client = client;
     }
 
     public void executeCommand() {
-        NodeId cmdExecId = nodeFactory.getCommandNodeMap().get("ExecuteMachineCommand");
+        NodeId nodeId = CommandNodes.EXECUTE_MACHINE_COMMAND.nodeId;
         Variant cmdExecVariant = new Variant(true);
         DataValue cmdExecDataValue = new DataValue(cmdExecVariant);
-        client.getOpcUaClient().writeValue(cmdExecId, cmdExecDataValue);
+        client.getOpcUaClient().writeValue(nodeId, cmdExecDataValue);
     }
 
     public void setCommand(Commands command) {
-        NodeId cmdNodeId = nodeFactory.getCommandNodeMap().get("SetMachineCommand");
+        NodeId nodeId = CommandNodes.SET_MACHINE_COMMAND.nodeId;
         Variant cmdVariant = new Variant(command.ordinal());
         DataValue cmdDataValue = new DataValue(cmdVariant);
-        client.getOpcUaClient().writeValue(cmdNodeId, cmdDataValue);
+        client.getOpcUaClient().writeValue(nodeId, cmdDataValue);
         executeCommand();
         String message = String.format("Executed command: %s %d", command,command.ordinal());
         LOGGER.log(Level.INFO,message);
