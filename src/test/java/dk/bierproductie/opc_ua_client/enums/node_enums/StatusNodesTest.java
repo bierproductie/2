fdd5c2887bcb71f3cafaabee3ecf875f97dcd8cc
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-09 17:18:18 +0100

    Added the rest of the enum tests

---
+++ src/test/java/dk/bierproductie/opc_ua_client/enums/node_enums/StatusNodesTest.java
@@ -0,0 +1,13 @@
+package dk.bierproductie.opc_ua_client.enums.node_enums;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatusNodesTest {
+
+    @Test
+    void values() {
+        assertNotNull(StatusNodes.values());
+    }
+}
