7e365ea8036378b762a13d4b48f5d085774738cf
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-09 23:14:22 +0100

    Update CommandNodes.java

--- src/main/java/dk/bierproductie/opc_ua_client/enums/node_enums/CommandNodes.java
+++ src/main/java/dk/bierproductie/opc_ua_client/enums/node_enums/CommandNodes.java
@@ -7,9 +7,9 @@ public enum CommandNodes {
     SET_MACHINE_SPEED(new NodeId(Constants.NAMESPACE_INDEX_VALUE, "::Program:Cube.Command.MachSpeed")),
     SET_MACHINE_COMMAND(new NodeId(Constants.NAMESPACE_INDEX_VALUE, "::Program:Cube.Command.CntrlCmd")),
     EXECUTE_MACHINE_COMMAND(new NodeId(Constants.NAMESPACE_INDEX_VALUE, "::Program:Cube.Command.CmdChangeRequest")),
-    NEXT_BATCH_ID(new NodeId(Constants.NAMESPACE_INDEX_VALUE, "::Program:Cube.Command.Parameter[0]")),
-    PRODUCT_ID_FOR_NEXT_BATCH(new NodeId(Constants.NAMESPACE_INDEX_VALUE, "::Program:Cube.Command.Parameter[1]")),
-    PRODUCT_AMOUNT_IN_NEXT_BATCH(new NodeId(Constants.NAMESPACE_INDEX_VALUE, "::Program:Cube.Command.Parameter[2]"));
+    SET_NEXT_BATCH_ID(new NodeId(Constants.NAMESPACE_INDEX_VALUE, "::Program:Cube.Command.Parameter[0].Value")),
+    SET_PRODUCT_ID_FOR_NEXT_BATCH(new NodeId(Constants.NAMESPACE_INDEX_VALUE, "::Program:Cube.Command.Parameter[1].Value")),
+    SET_PRODUCT_AMOUNT_IN_NEXT_BATCH(new NodeId(Constants.NAMESPACE_INDEX_VALUE, "::Program:Cube.Command.Parameter[2].Value"));
 
     public final NodeId nodeId;
 
