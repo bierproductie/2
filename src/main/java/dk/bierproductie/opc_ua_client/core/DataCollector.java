c8d32cf77310f1463b3c7cdffe46ec9c061106c6
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-09 18:56:46 +0100

    Update DataCollector.java

--- src/main/java/dk/bierproductie/opc_ua_client/core/DataCollector.java
+++ src/main/java/dk/bierproductie/opc_ua_client/core/DataCollector.java
@@ -4,6 +4,9 @@ import dk.bierproductie.opc_ua_client.enums.Commands;
 import dk.bierproductie.opc_ua_client.enums.MachineState;
 import dk.bierproductie.opc_ua_client.enums.Products;
 import dk.bierproductie.opc_ua_client.enums.StopReasons;
+import dk.bierproductie.opc_ua_client.enums.node_enums.AdminNodes;
+import dk.bierproductie.opc_ua_client.enums.node_enums.CommandNodes;
+import dk.bierproductie.opc_ua_client.enums.node_enums.StatusNodes;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
@@ -13,16 +16,16 @@ import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class MachineDataCollector {
+public class DataCollector {
 
     private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    public static final String INTERRUPTED_MESSAGE = "Interrupted!";
+    public static final String FORMAT = "%s : %d : %s";
 
     private Client client;
-    private NodeFactory nodeFactory;
 
-    public MachineDataCollector(Client client, NodeFactory nodeFactory) {
+    public DataCollector(Client client) {
         this.client = client;
-        this.nodeFactory = nodeFactory;
     }
 
     public void readData(String name, NodeId nodeId) {
@@ -32,68 +35,64 @@ public class MachineDataCollector {
             String message = String.format("%s : %s", name, String.valueOf(variant.getValue()));
             LOGGER.log(Level.INFO, message);
         } catch (InterruptedException | ExecutionException e) {
-            LOGGER.log(Level.WARNING, "Interrupted!", e);
+            LOGGER.log(Level.WARNING, INTERRUPTED_MESSAGE, e);
             Thread.currentThread().interrupt();
         }
     }
 
     public void readMachineState() {
-        String nodeName = "MachineState";
-        NodeId nodeId = nodeFactory.getStatusNodeMap().get(nodeName);
+        NodeId nodeId = StatusNodes.MACHINE_STATE.nodeId;
         try {
             DataValue dataValue = client.getOpcUaClient().readValue(0, TimestampsToReturn.Both, nodeId).get();
             Variant variant = dataValue.getValue();
             int stateInt = (int) variant.getValue();
-            String message = nodeName + " : " + stateInt + " : " + MachineState.values()[stateInt];
+            String message = String.format(FORMAT, StatusNodes.MACHINE_STATE, stateInt, MachineState.values()[stateInt]);
             LOGGER.log(Level.INFO, message);
         } catch (InterruptedException | ExecutionException e) {
-            LOGGER.log(Level.WARNING, "Interrupted!", e);
+            LOGGER.log(Level.WARNING, INTERRUPTED_MESSAGE, e);
             Thread.currentThread().interrupt();
         }
     }
 
 
     public void readStopReason() { // TODO: 08-11-2020 needs to me redone when commands are implemented
-        String nodeName = "StopReasonValue";
-        NodeId nodeId = nodeFactory.getAdminNodeMap().get(nodeName);
+        NodeId nodeId = AdminNodes.STOP_REASON_VALUE.nodeId;
         try {
             DataValue dataValue = client.getOpcUaClient().readValue(0, TimestampsToReturn.Both, nodeId).get();
             Variant variant = dataValue.getValue();
             int intValue = (int) variant.getValue();
-            String message = String.format("%s : %d : %s", nodeName, intValue, StopReasons.values()[intValue]);
+            String message = String.format(FORMAT, nodeId, intValue, StopReasons.values()[intValue]);
             LOGGER.log(Level.INFO, message);
         } catch (InterruptedException | ExecutionException e) {
-            LOGGER.log(Level.WARNING, "Interrupted!", e);
+            LOGGER.log(Level.WARNING, INTERRUPTED_MESSAGE, e);
             Thread.currentThread().interrupt();
         }
     }
 
     public void readCurrentCommand() {
-        String nodeName = "SetMachineCommand";
-        NodeId nodeId = nodeFactory.getCommandNodeMap().get(nodeName);
+        NodeId nodeId = CommandNodes.SET_MACHINE_COMMAND.nodeId;
         try {
             DataValue dataValue = client.getOpcUaClient().readValue(0, TimestampsToReturn.Both, nodeId).get();
             Variant variant = dataValue.getValue();
             int intValue = (int) variant.getValue();
-            String message = String.format("%s : %d : %s", nodeName, intValue, Commands.values()[intValue]);
+            String message = String.format(FORMAT, nodeId, intValue, Commands.values()[intValue]);
             LOGGER.log(Level.INFO, message);
         } catch (InterruptedException | ExecutionException e) {
-            LOGGER.log(Level.WARNING, "Interrupted!", e);
+            LOGGER.log(Level.WARNING, INTERRUPTED_MESSAGE, e);
             Thread.currentThread().interrupt();
         }
     }
 
     public void readCurrentProductId() {
-        String nodeName = "IDofProductInBatch";
-        NodeId nodeId = nodeFactory.getAdminNodeMap().get(nodeName);
+        NodeId nodeId = AdminNodes.BATCH_PRODUCT_ID.nodeId;
         try {
             DataValue dataValue = client.getOpcUaClient().readValue(0, TimestampsToReturn.Both, nodeId).get();
             Variant variant = dataValue.getValue();
             int intValue = Math.round((Float) variant.getValue());
-            String message = String.format("%s : %d : %s", nodeName, intValue, Products.values()[intValue]);
+            String message = String.format(FORMAT, nodeId, intValue, Products.values()[intValue]);
             LOGGER.log(Level.INFO,message);
         } catch (InterruptedException | ExecutionException e) {
-            LOGGER.log(Level.WARNING, "Interrupted!", e);
+            LOGGER.log(Level.WARNING, INTERRUPTED_MESSAGE, e);
             Thread.currentThread().interrupt();
         }
     }
@@ -106,7 +105,7 @@ public class MachineDataCollector {
             String message = nodeId.getIdentifier() + " : " + value;
             LOGGER.log(Level.INFO, message);
         } catch (InterruptedException | ExecutionException e) {
-            LOGGER.log(Level.WARNING, "Interrupted!", e);
+            LOGGER.log(Level.WARNING, INTERRUPTED_MESSAGE, e);
             Thread.currentThread().interrupt();
         }
     }
