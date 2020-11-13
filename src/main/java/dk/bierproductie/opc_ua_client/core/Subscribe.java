e6541388c28b3120dc609ef3b00257660ad901f1
Kechr <kechr19@student.sdu.dk>
2020-11-13 14:26:42 +0100

    subscribe method from SubscriptionHandler moved to Subscription.java

---
+++ src/main/java/dk/bierproductie/opc_ua_client/core/Subscribe.java
@@ -0,0 +1,72 @@
+package dk.bierproductie.opc_ua_client.core;
+
+import dk.bierproductie.opc_ua_client.enums.node_enums.StatusNodes;
+import dk.bierproductie.opc_ua_client.handlers.SubscriptionHandler;
+import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaMonitoredItem;
+import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaSubscription;
+import org.eclipse.milo.opcua.stack.core.AttributeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.UInteger;
+import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.MonitoringMode;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.TimestampsToReturn;
+import org.eclipse.milo.opcua.stack.core.types.structured.MonitoredItemCreateRequest;
+import org.eclipse.milo.opcua.stack.core.types.structured.MonitoringParameters;
+import org.eclipse.milo.opcua.stack.core.types.structured.ReadValueId;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.function.BiConsumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Subscribe {
+    private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+    public void subscribe(Client client) throws InterruptedException, ExecutionException {
+        NodeId nodeId = StatusNodes.CURRENT_BATCH_ID.nodeId;
+
+        // what to read
+        ReadValueId readValueId = new ReadValueId(nodeId, AttributeId.Value.uid(), null, null);
+
+        // important: client handle must be unique per item
+        UInteger clientHandle = Unsigned.uint(SubscriptionHandler.clientHandles.getAndIncrement());
+        //int clientHandle = 123456789;
+        MonitoringParameters parameters = new MonitoringParameters(
+                clientHandle,
+                1000.0,         // sampling interval
+                null,                   // filter, null means use default
+                Unsigned.uint(10),      // queue size
+                true              // discard oldest
+        );
+
+        // creation request
+        MonitoredItemCreateRequest request = new MonitoredItemCreateRequest(readValueId, MonitoringMode.Reporting, parameters);
+
+        // setting the consumer after the subscription creation
+        BiConsumer<UaMonitoredItem, Integer> onItemCreated = (item, id) -> item.setValueConsumer(SubscriptionHandler::onSubscriptionValue);
+
+        // create a subscription @ 1000ms
+        UaSubscription subscription = client.getOpcUaClient().getSubscriptionManager().createSubscription(1000.0).get();
+
+        List<UaMonitoredItem> items = subscription.createMonitoredItems(TimestampsToReturn.Both,
+                Arrays.asList(request),
+                onItemCreated).get();
+
+        for (UaMonitoredItem item : items) {
+            if (item.getStatusCode().isGood()) {
+                String msg = String.format("item created for nodeId=%s",
+                        item.getReadValueId().getNodeId());
+                LOGGER.log(Level.INFO, msg);
+            } else {
+                String msg = String.format("failed to create item for nodeId=%s (status=%s)",
+                        item.getReadValueId().getNodeId(), item.getStatusCode());
+                LOGGER.log(Level.INFO, msg);
+            }
+        }
+
+        // let the example run for 50 seconds then terminate
+        Thread.sleep(50000);
+    }
+}
--- src/main/java/dk/bierproductie/opc_ua_client/handlers/SubscriptionHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/SubscriptionHandler.java
@@ -1,81 +1,19 @@
 package dk.bierproductie.opc_ua_client.handlers;
 
-import dk.bierproductie.opc_ua_client.core.Client;
-import dk.bierproductie.opc_ua_client.enums.node_enums.StatusNodes;
 import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaMonitoredItem;
-import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaSubscription;
-import org.eclipse.milo.opcua.stack.core.AttributeId;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
-import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
-import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.UInteger;
-import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned;
-import org.eclipse.milo.opcua.stack.core.types.enumerated.MonitoringMode;
-import org.eclipse.milo.opcua.stack.core.types.enumerated.TimestampsToReturn;
-import org.eclipse.milo.opcua.stack.core.types.structured.MonitoredItemCreateRequest;
-import org.eclipse.milo.opcua.stack.core.types.structured.MonitoringParameters;
-import org.eclipse.milo.opcua.stack.core.types.structured.ReadValueId;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.BiConsumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SubscriptionHandler {
+    public static final AtomicLong clientHandles = new AtomicLong(1L);
     private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-    private static final AtomicLong clientHandles = new AtomicLong(1L);
 
-    private static void onSubscriptionValue(UaMonitoredItem item, DataValue value) {
+    public static void onSubscriptionValue(UaMonitoredItem item, DataValue value) {
         String msg = String.format("subscription value received: item=%s, value=%s",
                 item.getReadValueId().getNodeId(), value.getValue());
         LOGGER.log(Level.INFO, msg);
-    }
-
-    public void subscribe(Client client) throws InterruptedException, ExecutionException {
-        NodeId nodeId = StatusNodes.CURRENT_BATCH_ID.nodeId;
-
-        // what to read
-        ReadValueId readValueId = new ReadValueId(nodeId, AttributeId.Value.uid(), null, null);
-
-        // important: client handle must be unique per item
-        UInteger clientHandle = Unsigned.uint(clientHandles.getAndIncrement());
-        //int clientHandle = 123456789;
-        MonitoringParameters parameters = new MonitoringParameters(
-                clientHandle,
-                1000.0,         // sampling interval
-                null,                   // filter, null means use default
-                Unsigned.uint(10),      // queue size
-                true              // discard oldest
-        );
-
-        // creation request
-        MonitoredItemCreateRequest request = new MonitoredItemCreateRequest(readValueId, MonitoringMode.Reporting, parameters);
-
-        // setting the consumer after the subscription creation
-        BiConsumer<UaMonitoredItem, Integer> onItemCreated = (item, id) -> item.setValueConsumer(SubscriptionHandler::onSubscriptionValue);
-
-        // create a subscription @ 1000ms
-        UaSubscription subscription = client.getOpcUaClient().getSubscriptionManager().createSubscription(1000.0).get();
-
-        List<UaMonitoredItem> items = subscription.createMonitoredItems(TimestampsToReturn.Both,
-                Arrays.asList(request),
-                onItemCreated).get();
-
-        for (UaMonitoredItem item : items) {
-            if (item.getStatusCode().isGood()) {
-                String msg = String.format("item created for nodeId=%s",
-                        item.getReadValueId().getNodeId());
-                LOGGER.log(Level.INFO, msg);
-            } else {
-                String msg = String.format("failed to create item for nodeId=%s (status=%s)",
-                        item.getReadValueId().getNodeId(), item.getStatusCode());
-                LOGGER.log(Level.INFO, msg);
-            }
-        }
-
-        // let the example run for 50 seconds then terminate
-        Thread.sleep(50000);
     }
 }
