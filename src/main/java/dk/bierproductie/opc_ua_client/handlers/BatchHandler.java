4be9a846dc01cdd40bd595898afd976afcd48fd8
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-15 21:28:58 +0100

    BatchHandler sub setup

--- src/main/java/dk/bierproductie/opc_ua_client/handlers/BatchHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/BatchHandler.java
@@ -4,6 +4,7 @@ import dk.bierproductie.opc_ua_client.core.Batch;
 import dk.bierproductie.opc_ua_client.core.DataWriter;
 import dk.bierproductie.opc_ua_client.enums.Commands;
 import dk.bierproductie.opc_ua_client.enums.node_enums.CommandNodes;
+import dk.bierproductie.opc_ua_client.enums.node_enums.StatusNodes;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 
 import java.util.concurrent.ExecutionException;
@@ -12,17 +13,25 @@ public class BatchHandler {
 
     private CommandHandler commandHandler;
     private DataWriter dataWriter;
+    private SubscriptionHandler subscriptionHandler;
 
     public BatchHandler(OpcUaClient client) {
         this.commandHandler = new CommandHandler(client);
         this.dataWriter = new DataWriter(client);
+        this.subscriptionHandler = new SubscriptionHandler(client);
     }
 
     public void startBatch(Batch batch) throws ExecutionException, InterruptedException {
+        setupSubscriptions(batch);
         dataWriter.writeData(CommandNodes.SET_NEXT_BATCH_ID.nodeId, batch.getId());
         dataWriter.writeData(CommandNodes.SET_PRODUCT_ID_FOR_NEXT_BATCH.nodeId, batch.getProductType());
         dataWriter.writeData(CommandNodes.SET_PRODUCT_AMOUNT_IN_NEXT_BATCH.nodeId, batch.getAmountToProduce());
         dataWriter.writeData(CommandNodes.SET_MACHINE_SPEED.nodeId, batch.getMachineSpeed());
         commandHandler.setCommand(Commands.START);
+    }
+
+    public void setupSubscriptions(Batch batch) {
+        subscriptionHandler.subscribe(StatusNodes.TEMPERATURE.nodeId, batch, 50000);
+        //more idk
     }
 }
