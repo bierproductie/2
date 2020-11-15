f5f180af76bbbe63c387f888056bb9490a68f0a4
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-15 21:29:05 +0100

    Update SubscriptionHandler.java

--- src/main/java/dk/bierproductie/opc_ua_client/handlers/SubscriptionHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/SubscriptionHandler.java
@@ -1,18 +1,19 @@
 package dk.bierproductie.opc_ua_client.handlers;
 
+import dk.bierproductie.opc_ua_client.core.Batch;
 import dk.bierproductie.opc_ua_client.core.Subscription;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
 
 public class SubscriptionHandler {
-    private final OpcUaClient client;
+    private OpcUaClient client;
 
     public SubscriptionHandler(OpcUaClient client) {
         this.client = client;
     }
 
-    public void subscribe(NodeId nodeId, SubscriptionType subscriptionType) {
-        Thread subscriptionThread = new Thread(new Subscription(client, nodeId, subscriptionType));
+    public void subscribe(NodeId nodeId, Batch batch, long sleepTime) {
+        Thread subscriptionThread = new Thread(new Subscription(client, nodeId, batch, sleepTime));
         subscriptionThread.start();
     }
 }
