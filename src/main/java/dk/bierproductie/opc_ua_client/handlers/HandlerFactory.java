0777316b2ed56ba5b16771fea0f750d925c5ac05
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-18 18:36:04 +0100

    Created HandlerFactory to setup Handlers

---
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/HandlerFactory.java
@@ -0,0 +1,24 @@
+package dk.bierproductie.opc_ua_client.handlers;
+
+import dk.bierproductie.opc_ua_client.clients.MachineClient;
+import dk.bierproductie.opc_ua_client.clients.SimulatorClient;
+import dk.bierproductie.opc_ua_client.core.DataCollector;
+import dk.bierproductie.opc_ua_client.core.DataWriter;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+
+public class HandlerFactory {
+    private OpcUaClient client;
+
+    public HandlerFactory(boolean isSimulator) throws InterruptedException {
+        if (isSimulator){
+            client = SimulatorClient.getInstance().getOpcUaClient();
+        } else {
+            client = MachineClient.getInstance().getOpcUaClient();
+        }
+        CommandHandler.setInstance(client);
+        SubscriptionHandler.setInstance(client);
+        DataCollector.setInstance(client);
+        DataWriter.setInstance(client);
+        BatchHandler.setInstance(client);
+    }
+}
