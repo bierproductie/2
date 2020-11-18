f811f822614a6c5a8b41742d05fd578fcff7d6d5
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-18 18:36:08 +0100

    Update RunOpcUaClient.java

--- src/main/java/dk/bierproductie/opc_ua_client/RunOpcUaClient.java
+++ src/main/java/dk/bierproductie/opc_ua_client/RunOpcUaClient.java
@@ -5,6 +5,7 @@ import dk.bierproductie.opc_ua_client.core.Batch;
 import dk.bierproductie.opc_ua_client.core.OpcUaClient2;
 import dk.bierproductie.opc_ua_client.enums.Products;
 import dk.bierproductie.opc_ua_client.handlers.BatchHandler;
+import dk.bierproductie.opc_ua_client.handlers.HandlerFactory;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 
 import java.util.concurrent.ExecutionException;
@@ -18,8 +19,8 @@ public class RunOpcUaClient {
         LOGGER.log(Level.WARNING, "Application is fresh and ready to do some work! Lets get to it");
         new OpcUaClient2().start();
         LOGGER.log(Level.WARNING, "Application stopped...");
-        OpcUaClient opcUaClient = SimulatorClient.getInstance().getOpcUaClient();
-        BatchHandler batchHandler = new BatchHandler(opcUaClient);
+        HandlerFactory handlerFactory = new HandlerFactory(true);
+        BatchHandler batchHandler = BatchHandler.getInstance();
         Batch batch = new Batch(3, Products.PILSNER, 600, 100);
         batchHandler.startBatch(batch);
     }
