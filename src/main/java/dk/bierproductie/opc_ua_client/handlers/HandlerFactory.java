57636a5c9fcf88de77a2dfd2b0ee6f9b0fd289ce
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-19 13:16:38 +0100

    Update HandlerFactory.java

--- src/main/java/dk/bierproductie/opc_ua_client/handlers/HandlerFactory.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/HandlerFactory.java
@@ -6,9 +6,11 @@ import dk.bierproductie.opc_ua_client.core.DataCollector;
 import dk.bierproductie.opc_ua_client.core.DataWriter;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 
+import java.util.concurrent.ExecutionException;
+
 public class HandlerFactory {
 
-    public HandlerFactory(boolean isSimulator) throws InterruptedException {
+    public HandlerFactory(boolean isSimulator) throws InterruptedException, ExecutionException {
         OpcUaClient client;
         if (isSimulator){
             client = SimulatorClient.getInstance().getOpcUaClient();
@@ -19,6 +21,9 @@ public class HandlerFactory {
         SubscriptionHandler.setInstance(client);
         DataCollector.setInstance(client);
         DataWriter.setInstance(client);
+        MachineHandler machineHandler = new MachineHandler(client);
+        machineHandler.setUpSimulator();
+        Thread.sleep(1000);
         BatchHandler.setInstance();
     }
 }
