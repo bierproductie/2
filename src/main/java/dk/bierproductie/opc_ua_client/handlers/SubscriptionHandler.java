94c23ba13f20961a7339adbaf7c0ea1288ad734d
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-19 21:50:13 +0100

    undo all the stuff i did

--- src/main/java/dk/bierproductie/opc_ua_client/handlers/SubscriptionHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/SubscriptionHandler.java
@@ -1,17 +1,12 @@
 package dk.bierproductie.opc_ua_client.handlers;
 
-import dk.bierproductie.opc_ua_client.CLI;
 import dk.bierproductie.opc_ua_client.core.Subscription;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
-import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaSubscription;
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
-
-import java.util.ArrayList;
 
 public class SubscriptionHandler {
     private final OpcUaClient client;
     private static SubscriptionHandler instance;
-    private static ArrayList<UaSubscription> subscriptions;
 
     public SubscriptionHandler(OpcUaClient client) {
         this.client = client;
@@ -20,14 +15,6 @@ public class SubscriptionHandler {
     public void subscribe(NodeId nodeId, long sleepTime) {
         Thread subscriptionThread = new Thread(new Subscription(client, nodeId, sleepTime));
         subscriptionThread.start();
-    }
-
-    public static void addSubscription(UaSubscription uaSubscription) {
-        subscriptions.add(uaSubscription);
-    }
-
-    public void removeSubscriptions() {
-        client.getSubscriptionManager().clearSubscriptions();
     }
 
     public static void setInstance(OpcUaClient client) {
