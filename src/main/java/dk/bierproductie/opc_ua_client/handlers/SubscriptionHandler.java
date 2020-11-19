e2ce393801c07fe2a23a475c2d1d1ea3ba2ec4d8
Simon Jørgensen <sijo819@student.sdu.dk>
2020-11-19 21:14:17 +0100

    Update SubscriptionHandler.java

--- src/main/java/dk/bierproductie/opc_ua_client/handlers/SubscriptionHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/SubscriptionHandler.java
@@ -1,21 +1,33 @@
 package dk.bierproductie.opc_ua_client.handlers;
 
+import dk.bierproductie.opc_ua_client.CLI;
 import dk.bierproductie.opc_ua_client.core.Subscription;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaSubscription;
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+
+import java.util.ArrayList;
 
 public class SubscriptionHandler {
     private final OpcUaClient client;
     private static SubscriptionHandler instance;
+    private static ArrayList<UaSubscription> subscriptions;
 
     public SubscriptionHandler(OpcUaClient client) {
         this.client = client;
     }
 
-    public Thread subscribe(NodeId nodeId, long sleepTime) {
+    public void subscribe(NodeId nodeId, long sleepTime) {
         Thread subscriptionThread = new Thread(new Subscription(client, nodeId, sleepTime));
         subscriptionThread.start();
-        return subscriptionThread;
+    }
+
+    public static void addSubscription(UaSubscription uaSubscription) {
+        subscriptions.add(uaSubscription);
+    }
+
+    public void removeSubscriptions() {
+        client.getSubscriptionManager().clearSubscriptions();
     }
 
     public static void setInstance(OpcUaClient client) {
