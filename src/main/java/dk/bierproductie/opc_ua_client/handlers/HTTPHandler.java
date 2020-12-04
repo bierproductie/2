2ecf95b5cd2f2e7f366362d3f6077cb1746ac8f5
Simon Jørgensen <sijo819@student.sdu.dk>
2020-12-04 12:30:19 +0100

    Added postBatch that posts current batch

--- src/main/java/dk/bierproductie/opc_ua_client/handlers/HTTPHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/HTTPHandler.java
@@ -1,7 +1,10 @@
 package dk.bierproductie.opc_ua_client.handlers;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,11 +25,28 @@ public class HTTPHandler {
         }
     }
 
-    public static HTTPHandler getInstance(String urlString) {
+    public static HTTPHandler getInstance() {
         return instance;
     }
 
     public static void setInstance(String urlString) {
         HTTPHandler.instance = new HTTPHandler(urlString);
+    }
+
+    public void postBatch(){
+        try {
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type","application/json; utf-8");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+            try(OutputStream os = connection.getOutputStream()) {
+                byte[] input = BatchHandler.getCurrentBatch().json().getBytes("utf-8");
+                os.write(input, 0, input.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
     }
 }
