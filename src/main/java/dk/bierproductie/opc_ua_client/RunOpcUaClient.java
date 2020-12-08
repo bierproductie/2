4c84b313d8e68638a86bf29d696eb78f967f4d0e
Simon Jørgensen <sijo819@student.sdu.dk>
2020-12-08 23:02:39 +0100

    Update RunOpcUaClient.java
    new test

--- src/main/java/dk/bierproductie/opc_ua_client/RunOpcUaClient.java
+++ src/main/java/dk/bierproductie/opc_ua_client/RunOpcUaClient.java
@@ -6,6 +6,7 @@ import dk.bierproductie.opc_ua_client.enums.Products;
 import dk.bierproductie.opc_ua_client.handlers.BatchHandler;
 import dk.bierproductie.opc_ua_client.handlers.HandlerFactory;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,6 +21,18 @@ public class RunOpcUaClient {
         HandlerFactory.getInstance(true);
         BatchHandler batchHandler = BatchHandler.getInstance();
         Batch batch = new Batch(3, Products.PILSNER, 600, 300);
-        batchHandler.startBatch(batch);
+        Batch batch1 = new Batch(4, Products.WHEAT, 300, 150);
+        Batch batch2 = new Batch(53, Products.ALE, 100, 50);
+        ArrayList<Batch> batches = new ArrayList<>();
+        batches.add(batch);
+        batches.add(batch1);
+        batches.add(batch2);
+        for (Batch bth : batches){
+            batchHandler.startBatch(bth);
+            while (BatchHandler.getCurrentBatch().isRunning()) {
+                Thread.sleep(1000);
+            }
+            System.out.println("finished batch" + bth.getId());
+        }
     }
 }
