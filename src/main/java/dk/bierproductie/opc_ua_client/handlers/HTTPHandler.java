f5d7a448ed15eb4eb995c6095d46c78ff8d71026
Simon Jørgensen <sijo819@student.sdu.dk>
2020-12-08 23:02:27 +0100

    dynamic post method

--- src/main/java/dk/bierproductie/opc_ua_client/handlers/HTTPHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/HTTPHandler.java
@@ -2,10 +2,11 @@ package dk.bierproductie.opc_ua_client.handlers;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,9 +20,9 @@ public class HTTPHandler {
     public HTTPHandler(String urlString) {
         try {
             URL url = new URL(urlString);
-            connection = (HttpURLConnection)url.openConnection();
+            connection = (HttpURLConnection) url.openConnection();
         } catch (IOException e) {
-            LOGGER.log(Level.SEVERE,"HTTPManger connection not established");
+            LOGGER.log(Level.SEVERE, "HTTPManger connection not established");
         }
     }
 
@@ -33,19 +34,39 @@ public class HTTPHandler {
         HTTPHandler.instance = new HTTPHandler(urlString);
     }
 
-    public void postBatch(){
+    public void postBatch() {
         try {
             connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type","application/json; utf-8");
+            connection.setRequestProperty("Content-Type", "application/json; utf-8");
             connection.setRequestProperty("Accept", "application/json");
             connection.setDoOutput(true);
-            try(OutputStream os = connection.getOutputStream()) {
-                byte[] input = BatchHandler.getCurrentBatch().json().getBytes("utf-8");
-                os.write(input, 0, input.length);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            writeOutput(BatchHandler.getCurrentBatch().json());
         } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void postData(String data, String endpoint) {
+        String urls = connection.getURL().getHost() + endpoint;
+        System.out.println(urls);
+        try {
+            URL url = new URL(urls);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json; utf-8");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+            writeOutput(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeOutput(String data) {
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = data.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
