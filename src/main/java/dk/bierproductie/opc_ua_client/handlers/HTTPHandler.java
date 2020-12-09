a320f850907407705ed5d1ab8c1524d3aa0188b2
Simon Jørgensen <sijo819@student.sdu.dk>
2020-12-09 13:28:49 +0100

    Update HTTPHandler.java

--- src/main/java/dk/bierproductie/opc_ua_client/handlers/HTTPHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/HTTPHandler.java
@@ -1,12 +1,15 @@
 package dk.bierproductie.opc_ua_client.handlers;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -46,19 +49,35 @@ public class HTTPHandler {
         }
     }
 
-    public void postData(String data, String endpoint) {
-        String urls = connection.getURL().getHost() + endpoint;
-        System.out.println(urls);
-        try {
-            URL url = new URL(urls);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Content-Type", "application/json; utf-8");
-            con.setRequestProperty("Accept", "application/json");
-            con.setDoOutput(true);
-            writeOutput(data);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void postData(String data, long time, String endpoint, boolean isSimulating) {
+        System.out.println(data);
+        System.out.println(endpoint);
+        Date d = new Date(time);
+        System.out.println(d);
+        if (HandlerFactory.onSimulator && !isSimulating) {
+            simulateOutput(endpoint,time);
+        }
+//        String urls = connection.getURL().getHost() + endpoint;
+//        System.out.println(urls);
+//        try {
+//            URL url = new URL(urls);
+//            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//            con.setRequestMethod("POST");
+//            con.setRequestProperty("Content-Type", "application/json; utf-8");
+//            con.setRequestProperty("Accept", "application/json");
+//            con.setDoOutput(true);
+//            writeOutput(data);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public void simulateOutput(String endpoint, long time){
+        Random r = new Random();
+        Gson gson = new Gson();
+        if (endpoint == "producedProducts") {
+            float random = (float) (20 + r.nextFloat() * (20.5 - 20));
+            postData(gson.toJson(random),time,"temperature",true);
         }
     }
 
