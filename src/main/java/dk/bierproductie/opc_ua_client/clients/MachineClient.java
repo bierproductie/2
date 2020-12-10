ec8465185eebf3558c64128fd1c4e70677b69575
Simon Jørgensen <sijo819@student.sdu.dk>
2020-12-10 23:27:52 +0100

    Update MachineClient.java
    Fixed machine client. guess we wouldnt know if it worked :>

--- src/main/java/dk/bierproductie/opc_ua_client/clients/MachineClient.java
+++ src/main/java/dk/bierproductie/opc_ua_client/clients/MachineClient.java
@@ -4,7 +4,9 @@ import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfigBuilder;
 import org.eclipse.milo.opcua.stack.client.DiscoveryClient;
 import org.eclipse.milo.opcua.stack.core.UaException;
+import org.eclipse.milo.opcua.stack.core.types.structured.EndpointConfiguration;
 import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
+import org.eclipse.milo.opcua.stack.core.util.EndpointUtil;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -18,13 +20,14 @@ public final class MachineClient {
 
     private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-    public MachineClient(String endpointURL) throws InterruptedException {
+    public MachineClient(String url, int port) throws InterruptedException {
         try {
-            List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints(endpointURL).get();
+            List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints(url + ":" + port).get();
             LOGGER.log(Level.INFO, "Connecting to Endpoint: {}", endpoints.get(0));
+            EndpointDescription endDesc = EndpointUtil.updateUrl(endpoints.get(0), url, port);
 
             OpcUaClientConfigBuilder ocb = new OpcUaClientConfigBuilder();
-            ocb.setEndpoint(endpoints.get(0));
+            ocb.setEndpoint(endDesc);
 
             opcUaClient = OpcUaClient.create(ocb.build());
             opcUaClient.connect().get();
@@ -35,9 +38,9 @@ public final class MachineClient {
         }
     }
 
-    public static MachineClient getInstance() throws InterruptedException {
+    public static MachineClient getInstance(String url, int port) throws InterruptedException {
         if (instance == null) {
-            instance = new MachineClient("opc.tcp://127.0.0.1:4840");
+            instance = new MachineClient(url, port);
         }
         return instance;
     }
