c92a3cb7c15075f268e4ee2d787ce894c0cf20a7
Simon Jørgensen <sijo819@student.sdu.dk>
2020-12-13 00:24:38 +0100

    Update HTTPHandler.java

--- src/main/java/dk/bierproductie/opc_ua_client/handlers/HTTPHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/HTTPHandler.java
@@ -1,36 +1,23 @@
 package dk.bierproductie.opc_ua_client.handlers;
 
-import com.google.gson.Gson;
+import com.squareup.okhttp.*;
+import dk.bierproductie.opc_ua_client.core.BatchData;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class HTTPHandler {
 
+    private static final MediaType JSON = MediaType.parse("application/json");
     private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     private static HTTPHandler instance;
-    private HttpURLConnection connection;
+    private String apiUrl;
 
     public HTTPHandler() {
         ConfigHandler configHandler = ConfigHandler.getInstance();
-
-        String apiUrl = configHandler.getApiUrl();
-
-        try {
-            URL url = new URL(apiUrl);
-            connection = (HttpURLConnection) url.openConnection();
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "HTTPManger connection not established");
-        }
+        apiUrl = configHandler.getApiUrl();
     }
 
     public static HTTPHandler getInstance() {
@@ -40,54 +27,20 @@ public class HTTPHandler {
         return instance;
     }
 
-    public void postBatch() {
+
+    public void postData() {
+        BatchData batchData = BatchHandler.getCurrentBatchData();
+        OkHttpClient client = new OkHttpClient();
+        String json = batchData.toJson();
+        System.out.println(json);
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(apiUrl)
+                .post(body)
+                .build();
         try {
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json; utf-8");
-            connection.setRequestProperty("Accept", "application/json");
-            connection.setDoOutput(true);
-            writeOutput(BatchHandler.getCurrentBatch().json());
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void postData(String data, long time, String endpoint, boolean isSimulating) {
-        System.out.println(data);
-        System.out.println(endpoint);
-        Date d = new Date(time);
-        System.out.println(d);
-        if (HandlerFactory.onSimulator && !isSimulating) {
-            simulateOutput(endpoint, time);
-        }
-//        String urls = connection.getURL().getHost() + endpoint;
-//        System.out.println(urls);
-//        try {
-//            URL url = new URL(urls);
-//            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-//            con.setRequestMethod("POST");
-//            con.setRequestProperty("Content-Type", "application/json; utf-8");
-//            con.setRequestProperty("Accept", "application/json");
-//            con.setDoOutput(true);
-//            writeOutput(data);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-    }
-
-    public void simulateOutput(String endpoint, long time) {
-        Random r = new Random();
-        Gson gson = new Gson();
-        if (endpoint == "producedProducts") {
-            float random = (float) (20 + r.nextFloat() * (20.5 - 20));
-            postData(gson.toJson(random), time, "temperature", true);
-        }
-    }
-
-    public void writeOutput(String data) {
-        try (OutputStream os = connection.getOutputStream()) {
-            byte[] input = data.getBytes(StandardCharsets.UTF_8);
-            os.write(input, 0, input.length);
+            Response response = client.newCall(request).execute();
+            LOGGER.log(Level.INFO,response.body().toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
