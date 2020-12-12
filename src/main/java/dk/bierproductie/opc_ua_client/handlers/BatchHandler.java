7614bdc3ba00fe192c71885cf09002b7337e6357
Simon Jørgensen <sijo819@student.sdu.dk>
2020-12-13 00:53:31 +0100

    Update BatchHandler.java

--- src/main/java/dk/bierproductie/opc_ua_client/handlers/BatchHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/BatchHandler.java
@@ -87,11 +87,11 @@ public final class BatchHandler {
     }
 
     public void setupConstantSubscriptions() {
-//        subscriptionHandler.subscribe(StatusNodes.TEMPERATURE.nodeId, true);
-//        subscriptionHandler.subscribe(StatusNodes.MACHINE_STATE.nodeId,true);
-//        subscriptionHandler.subscribe(StatusNodes.HUMIDITY.nodeId,true);
-//        subscriptionHandler.subscribe(StatusNodes.VIBRATION.nodeId,true);
-//        subscriptionHandler.subscribe(AdminNodes.DEFECTIVE_PRODUCTS.nodeId,true);
+        subscriptionHandler.subscribe(StatusNodes.TEMPERATURE.nodeId, true);
+        subscriptionHandler.subscribe(StatusNodes.MACHINE_STATE.nodeId,true);
+        subscriptionHandler.subscribe(StatusNodes.HUMIDITY.nodeId,true);
+        subscriptionHandler.subscribe(StatusNodes.VIBRATION.nodeId,true);
+        subscriptionHandler.subscribe(AdminNodes.DEFECTIVE_PRODUCTS.nodeId,true);
         subscriptionHandler.subscribe(AdminNodes.PRODUCED_PRODUCTS.nodeId,true);
     }
 
