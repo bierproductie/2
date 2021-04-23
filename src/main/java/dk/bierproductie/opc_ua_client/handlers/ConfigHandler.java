4a9aef92b122c8b444d15794b98769319924c7bd
kepet19 <kevinmeejach@gmail.com>
2021-04-23 14:33:21 +0200

    Made ConfigHandler be more verbose ind loading varibles

--- src/main/java/dk/bierproductie/opc_ua_client/handlers/ConfigHandler.java
+++ src/main/java/dk/bierproductie/opc_ua_client/handlers/ConfigHandler.java
@@ -1,14 +1,36 @@
 package dk.bierproductie.opc_ua_client.handlers;
 
-public class ConfigHandler {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class ConfigHandler {
+    private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     private static ConfigHandler instance;
-    private String apiUrl = System.getenv("API_URL");
-    private String machineUrl = System.getenv("MACHINE_URL");
-    private String machinePort = System.getenv("MACHINE_PORT");
-    private String simUrl = System.getenv("SIMULATOR_URL");
-    private String simUser = System.getenv("SIMULATOR_USER");
-    private String simPwd = System.getenv("SIMULATOR_PASSWORD");
+    private String apiUrl = "API_URL";
+    private String machineUrl = "MACHINE_URL";
+    private String machinePort = "MACHINE_PORT";
+    private String simUrl = "SIMULATOR_URL";
+    private String simUser = "SIMULATOR_USER";
+    private String simPwd = "SIMULATOR_PASSWORD";
+
+    private ConfigHandler(){
+        apiUrl = getEnv(apiUrl);
+        machineUrl = getEnv(machineUrl);
+        machinePort = getEnv(machinePort);
+        simUrl = getEnv(simUrl);
+        simUser = getEnv(simUser);
+        simPwd = getEnv(simPwd);
+    }
+    private String getEnv(String envStr){
+        String env = System.getenv(envStr);
+        if (env == null) {
+            LOGGER.log(Level.WARNING, "envStr, ${0} is null", envStr);
+            return env;
+        }
+        LOGGER.log(Level.INFO, "Enviroment variable , ${0} load sucessfull", envStr);
+        return env;
+    }
+
 
     public static ConfigHandler getInstance() {
         if (instance == null) {
